4e37b727c715b449f28a41132f44d76f4aa05a4a
KamCoder5 <KamilahMohchin@gmail.com>
2022-06-29 21:13:40 +0100

    Delete icon conditionally renders

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,18 +1,35 @@
 import { RiDeleteBin5Fill } from "react-icons/ri";
+import { useState } from "react";
 function Memo(props) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleOnMouseEnterDeleteIcon() {
+    setIsVisible(!isVisible);
+  }
+
+  function handleOnMouseLeaveDeleteIcon() {
+    setIsVisible(!isVisible);
+  }
+
   return (
-    <div className="memo">
+    <div
+      className="memo"
+      onMouseEnter={handleOnMouseEnterDeleteIcon}
+      onMouseLeave={handleOnMouseLeaveDeleteIcon}
+    >
       <div className="memo-title">{props.title} </div>
       <div className="memo-body">{props.body}</div>
       <div className="memo-footer">
         <small>{props.creation_date}</small>
-        <RiDeleteBin5Fill
-          onClick={() => {
-            props.handleDeleteNote(props.id);
-          }}
-          className="delete-icon"
-          size="1.3em"
-        />
+        {isVisible ? (
+          <RiDeleteBin5Fill
+            className="delete-icon"
+            size="1.3em"
+            onClick={() => {
+              props.handleDeleteNote(props.id);
+            }}
+          />
+        ) : null}
       </div>
     </div>
   );
